64894252245a9c92be7790dad11786030a09961a
nachiket273 <nachiket.tanksale@gmail.com>
2024-09-04 01:16:26 +0530

    Added add function

--- src/value.rs
+++ src/value.rs
@@ -1,79 +1,102 @@
 use std::{
-    borrow::Borrow, fmt::Debug, hash::Hash, ops::Add
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    fmt::Debug,
+    hash::Hash,
+    ops::{Add, Deref},
+    rc::Rc
 };
+use uuid::Uuid;
 
 // Basic Implementation of struct Value
-#[derive(Clone)]
-struct Value {
-    data: f64,
-    grad: f64,
-    operation: String,
-    label: String
+#[derive(Debug)]
+pub struct Value {
+    pub data: f64,
+    pub grad: f64,
+    pub operation: String,
+    pub backward: Option<fn(val: &Value)>,
+    pub previous: Vec<Val>,
+    pub id: Uuid
 }
 
 impl Value {
-    fn new(data: f64, grad: f64,
-        operation: String,
-        label: String) -> Value {
-            Value {
-                data, grad, operation, label
-            }
+    fn new(data: f64) -> Value {
+        Value {
+            data,
+            grad: 0.0,
+            operation: "".to_string(),
+            backward: None,
+            previous: Vec::new(),
+            id: Uuid::new_v4()
         }
+    }
 }
 
-impl PartialEq for Value {
+#[derive(Clone)]
+pub struct Val(Rc<RefCell<Value>>);
+
+impl Deref for Val {
+    type Target = Rc<RefCell<Value>>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl PartialEq for Val {
     fn eq(&self, other: &Self) -> bool {
-        self.data == other.data
-        && self.grad == other.grad
-        && self.operation == other.operation
-        && self.label == other.label
+        self.borrow().id == other.borrow().id
     }
 }
 
-impl Eq for Value {}
+impl Eq for Val{}
 
-impl Hash for Value {
+impl Hash for Val {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.data.to_bits().hash(state);
-        self.grad.to_bits().hash(state);
-        self.operation.hash(state);
-        self.label.hash(state);
+        self.borrow().id.hash(state)
     }
 }
 
-impl Debug for Value {
+impl Debug for Val {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Value")
-        .field("data", &self.data)
-        .field("grad", &self.grad)
-        .field("operation", &self.operation)
-        .field("label", &self.label)
+        .field("data", &self.borrow().data)
+        .field("grad", &self.borrow().grad)
+        .field("operation", &self.borrow().operation)
         .finish()
     }
 }
 
-// Implementation of Add, Subtract, Multiply and Divide
-impl Add<Value> for Value {
-    type Output = Value;
 
-    fn add(self, other: Value) -> Self::Output {
-        add(&self, &other)
+impl Val {
+    fn new(value: Value) -> Val {
+        Val(Rc::new(RefCell::new(value)))
     }
 }
 
-fn add(a: &Value, b: &Value) -> Value {
-    let addn = a.borrow().data + b.borrow().data;
-
-    Value::new(addn, 0.0,
-        "+".to_string(),
-        "addition".to_string())
+impl<T: Into<f64>> From<T> for Val {
+    fn from(t: T) -> Val {
+        Val::new(Value::new(t.into()))
+    }
 }
 
+// Implementation of Add, Subtract, Multiply and Divide
+impl Add for Val {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut add_val = Value::new(self.borrow().data + other.borrow().data);
+        add_val.operation = String::from("+");
+        add_val.previous = vec![self, other];
+        add_val.backward = Some(|val: &Value| {
+            val.previous[0].borrow_mut().grad += val.grad;
+            val.previous[1].borrow_mut().grad += val.grad;
+        });
+        Val::new(add_val)
+    }
+}
 
 fn main() {
-    let val = Value::new(10.0, 0.0, "".to_string(),
-    "Val1".to_string());
-    let val2 = Value::clone(&val);
+    let val = Val::from(10.0);
+    let val2 = Val::clone(&val);
     println!("Current struct: {:#?}", val);
-    assert!(val.eq(&val2), "Not True");
 }  
